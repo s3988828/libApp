d0310fc0e2bc942235c8cc63b8d23a82f2313c78
SEF-Assessment4 <sheen2906@gmail.com>
2024-05-30 03:58:07 +1000

    new instance

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://13.211.168.153:5000', //  Flask server's address
+    baseURL: 'http://3.107.59.62:5000', //  Flask server's address
 });
 
 export default api;
