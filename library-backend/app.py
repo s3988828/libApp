29db428fbebf7ce86ff466ec6e795cd74d330aac
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-02 08:36:15 +1000

    new role

--- library-backend/app.py
+++ library-backend/app.py
@@ -66,7 +66,7 @@ def login():
 
     if user and bcrypt.check_password_hash(user['password'], password):
         access_token = create_access_token(identity={'username': user['username'], 'role': user['role']})
-        return jsonify(access_token=access_token), 200
+        return jsonify(access_token=access_token, user_role=user['role']), 200
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
