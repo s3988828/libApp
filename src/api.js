9c9adca76b09eb67bc031f6049c6885412ac5b20
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-02 06:07:19 +1000

    flask fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ec2-3-106-206-227.ap-southeast-2.compute.amazonaws.com/api',
+    baseURL: 'https://ec2-3-27-159-85.ap-southeast-2.compute.amazonaws.com/api',
 });
 
 export default api;
