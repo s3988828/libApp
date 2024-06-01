e04d70d1d623fc893e5d023362edbfd9e1af8d53
SEF-Assessment4 <sheen2906@gmail.com>
2024-06-02 03:05:46 +1000

    final upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ec2-3-27-159-85.ap-southeast-2.compute.amazonaws.com',
+    baseURL: 'http://3.27.159.85',
 });
 
 export default api;
